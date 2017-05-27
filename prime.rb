745f4a3898352f9a544d21c8e6a05089bef8f04d
Panagiotis Papadimitropoulos <ppapadim12@gmail.com>
2017-05-27 18:34:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+
+return false if num < 2
+return true if num == 2
+
+if num > 2
+  divisable_vals = 0
+  (2...num).to_a.each { |element| divisable_vals += 1 if num % element == 0 }
+
+  return true if divisable_vals == 0
+  return false if divisable_vals != 0
+end
+
+
+end
